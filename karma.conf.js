199208fcc4649a7c0823d60f4cdfe3382efcdfc7
Armando Acuña <amagi6272@gmail.com>
2016-05-27 13:45:06 -0700

    tests, etc

--- karma.conf.js
+++ karma.conf.js
@@ -1,70 +1,37 @@
-// Karma configuration
-// Generated on Fri May 27 2016 00:29:42 GMT-0700 (MST)
-var webpackCongif = require('./webpack.config');
+var path = require('path');
+var webpackConfig = require('./webpack.config');
 
 module.exports = function(config) {
-  config.set({
+    config.set({
+        basePath: '',
+        frameworks: ['mocha', 'chai'],
+        files: [
+            'tests.webpack.js'
+        ],
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+        preprocessors: {
+            // add webpack as preprocessor
+            'tests.webpack.js': ['webpack', 'sourcemap'],
+        },
 
+        webpack: webpackConfig,
+        webpackServer: {
+            noInfo: true
+        },
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'chai'],
+        plugins: [
+            'karma-mocha',
+            'karma-chai',
+            'karma-webpack',
+            'karma-phantomjs-launcher',
+            'karma-spec-reporter',
+            'karma-sourcemap-loader'
+        ],
 
-
-    // list of files / patterns to load in the browser
-    files: [
-    ],
-
-
-    // list of files to exclude
-    exclude: [
-    ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
-
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity,
-    plugins: ['karma-mocha', 'karma-chai', 'karma-webpack']
-  });
+        reporters: ['spec'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        browsers: ['PhantomJS']
+    })
 };
--- src/app.js
+++ src/app.js
@@ -3,20 +3,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './app.css';
-import styles from './styles.module.css';
-import 'font-awesome/css/font-awesome.css';
+//import styles from './styles.module.css';
+//import 'font-awesome/css/font-awesome.css';
 
-const App = React.createClass({
-    render: function() {
-        return (
-            <div className={styles.wrapper}>
-                <i className="fa fa-star"></i>
-                <h1>Environment: {__NODE_ENV__}</h1>
-
-            </div>
-        )
-    }
-});
+import App from './containers/App/App.js';
 
 const mountNode = document.querySelector('#root');
 ReactDOM.render(<App />, mountNode);
